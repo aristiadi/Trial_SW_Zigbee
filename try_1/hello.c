48782f56c75cc4fd45f37ef1ab7b7025aa5440aa
Agus Reza <agus.btp@gmail.com>
2013-10-03 11:53:22 +0700

    ternyata tadi belum ke-upload
    oke, sekarang mari kita upload :D

---
+++ try_1/hello.c
@@ -0,0 +1,45 @@
+/*
+	filename	: hello.c
+	kegunaan	: mengetes cc2530, menyalakan lampu led kedip-kedip
+				  dengan interval waktu 50 ms
+*/	
+
+/***********************************************************************************
+* INCLUDES
+*/
+
+#include <hal_lcd.h>
+#include <hal_led.h>
+#include <hal_joystick.h>
+#include <hal_board.h>
+#include <hal_int.h>
+#include <hal_assert.h>
+#include "hal_rf.h"
+#include "util.h"
+#include "hal_rf_util.h"
+#include "util_lcd.h"
+
+void main(void)
+{
+    // Initalise board peripherals
+    halBoardInit();
+
+    // Initalise hal_rf
+    if(halRfInit()==FAILED) {
+      HAL_ASSERT(FALSE);
+    }
+    // Indicate that device is powered
+    halLedSet(1);
+
+    // Print Logo and splash screen on LCD
+    utilPrintLogo("Agus Reza");
+    halMcuWaitMs(3000);
+
+
+    while (1) {
+    halLedToggle(3);
+    halMcuWaitMs(50);
+    }
+
+}
+
